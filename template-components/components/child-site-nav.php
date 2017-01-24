4ca69d033fc808cbcdf7d566648e08cdd9f9718a
Zsolt Revay <zsolt.revay.g@gmail.com>
2017-01-24 17:21:33 +0200

    Remove pluggable from child theme

--- template-components/components/child-site-nav.php
+++ template-components/components/child-site-nav.php
@@ -7,46 +7,31 @@
  * @package wps_prime
  */
 
-if ( ! function_exists( 'main_site_nav' ) ) {
-	/**
-	 * Main site navigation
-	 * mobile navigation toggle button
-	 */
-	function main_site_nav() {
-
-		echo '<nav id="site-nav"'. main_nav_class() .' role="navigation" data-ui-component="site-main-nav">';
-
-		wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-nav__list', 'walker' => new Theme_Menu_Object() ) );
-		echo '</nav><!-- #site-nav -->';
-
-	}
+/**
+ * Main site navigation
+ * mobile navigation toggle button
+ */
+function main_site_nav() {
+	echo '<nav id="site-nav"'. main_nav_class() .' role="navigation" data-ui-component="site-main-nav">';
+	wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-nav__list', 'walker' => new Theme_Menu_Object) ) );
+	echo '</nav><!-- #site-nav -->';
 }
 
-if ( ! function_exists( 'main_site_nav_mobile' ) ) {
-	/**
-	 * Main site navigation
-	 * mobile navigation toggle button
-	 */
-	function main_site_nav_mobile() {
 
-		echo '<nav id="site-mobile-nav"'. main_mobile_nav_class() .' role="navigation-mobile" data-ui-component="site-main-nav-mobile">';
-
-		wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-mobile-nav__list', 'walker' => new Theme_Menu_Object() ) );
-		echo '</nav><!-- #site-mobile-nav -->';
-
-	}
+/**
+ * Main site navigation
+ * mobile navigation toggle button
+ */
+function main_site_nav_mobile() {
+	echo '<nav id="site-mobile-nav"'. main_mobile_nav_class() .' role="navigation-mobile" data-ui-component="site-main-nav-mobile">';
+	wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-mobile-nav__list', 'walker' => new Theme_Menu_Object() ) );
+	echo '</nav><!-- #site-mobile-nav -->';
 }
 
-if ( ! function_exists( 'main_site_mobile_nav_toggler' ) ) {
 
 function main_site_mobile_nav_toggler() {
-
-		$ico = '<div class="site-mobile-nav-icon"><span></span><span></span><span></span><span></span></div>';
-
-		echo '<button class="site-mobile-nav-toggler"  id="nav-toggler" data-ui-component="menu-toggle-button">';
-		echo $ico;
-		echo '</button>';
-
-		}
-
+	$ico = '<div class="site-mobile-nav-icon"><span></span><span></span><span></span><span></span></div>';
+	echo '<button class="site-mobile-nav-toggler"  id="nav-toggler" data-ui-component="menu-toggle-button">';
+	echo $ico;
+	echo '</button>';
 }
