a94334633f1f1f153bcec590ca50a673f0fc1260
Zsolt Revay <zsolt.revay.g@gmail.com>
2017-02-17 11:54:56 +0200

    Update mobile nav with mobile menu walker

--- template-components/components/child-site-nav.php
+++ template-components/components/child-site-nav.php
@@ -23,14 +23,14 @@ function wps_main_site_nav() {
  * mobile navigation toggle button
  */
 function wps_main_site_nav_mobile() {
-	echo '<nav id="site-mobile-nav"'. wps_site_mobile_nav_class() .' role="navigation-mobile" data-ui-component="site-main-nav-mobile">';
-	wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-mobile-nav__list',  'container_class'=>'site-nav-mobile__menu-container', 'walker' => new Theme_Menu_Object() ) );
+	echo '<nav id="site-mobile-nav"'. wps_site_mobile_nav_class() .' data-ui-component="site-main-nav-mobile">';
+	wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-mobile-nav__list',  'container_class'=>'site-nav-mobile__menu-container', 'walker' => new Theme_Mobile_Menu_Object() ) );
 	echo '</nav><!-- #site-mobile-nav -->';
 }
 
 
 function wps_main_site_mobile_nav_toggler() {
-	$ico = '<div class="site-mobile-nav-icon"><span></span><span></span><span></span><span></span></div>';
+	$ico = '<span class="site-mobile-nav-icon"><span></span><span></span><span></span><span></span></span>';
 	echo '<button class="site-mobile-nav-toggler"  id="nav-toggler" data-ui-component="menu-toggle-button">';
 	echo $ico;
 	echo '</button>';
